477e1a8f694e5723c6f5ec396610ff2b2a03274b
orielbachar <oriel.bachar@gmail.com>
2017-01-12 12:44:24 +0200

    mp3 in ios

--- public/js/service/podcastService.js
+++ public/js/service/podcastService.js
@@ -24,7 +24,7 @@ app.factory('recordingService', ['$http' , 'authService', function($http, authSe
             for (var i = 0; i < recordingService.recordings.length; i++) {
               recording = recordingService.recordings[i].link;
               recording = "api.twilio.com" + recording.replace('json', 'mp3');
-              recording = 'https://{ACe175fbe84cb43b81742d9e9516c751af}:{fce5bbd7a29d1f57c19c4463b7694a03}@' + recording;
+              recording = 'https://ACe175fbe84cb43b81742d9e9516c751af:fce5bbd7a29d1f57c19c4463b7694a03@' + recording;
               recordingService.records.push(recording);
             }
             return recordingService.records;
